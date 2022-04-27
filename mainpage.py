0045a94b68e07c2d0b64e0dfc67c57e333fca2c4
MooMoonica <72373846+MooMoonica@users.noreply.github.com>
2022-04-26 23:29:08 -0700

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -25,18 +25,32 @@ class RegisterPage(Screen):
 class LogInPage(Screen):
 	pass
 
-class ViewPage(Screen):
+
+class CoachPage(Screen):
+	pass
+
+class TeamPage(Screen):
+	pass
+
+class PlayerPage(Screen):
+	pass
+
+class AddPlayers(Screen):
 	pass
 
 smanager = ScreenManager()
 smanager.add_widget(MainPage(name = 'TeamX'))
 smanager.add_widget(RegisterPage(name = 'Registering...'))
 smanager.add_widget(LogInPage(name = 'LogIn'))
-smanager.add_widget(ViewPage(name = 'Viewing'))
+smanager.add_widget(CoachPage(name = 'Coach'))
+smanager.add_widget(TeamPage(name = 'Team'))
+smanager.add_widget(PlayerPage(name = 'Player'))
+smanager.add_widget(AddPlayers(name = 'Add_Players'))
+
 
 class TeamXApp(MDApp):
 	def build(self):
-		Window.size = [500, 800]
+		Window.size = [1200, 800]
 		screen = Builder.load_string(AppPagesAssets)
 		
 		self.theme_cls.theme_style = "Light"
@@ -102,6 +116,44 @@ class TeamXApp(MDApp):
 			
 		return screen
 	
+	def add(self):
+		#define the database
+		DB_HOST = "/tmp"
+		DB_NAME = "moonica"
+		DB_USER = "moonica"
+		DB_PASS = "0010"
+		DB_PORT = "8888"
+
+		#create a database or connect to one
+		conn = psycopg2.connect(
+			host = DB_HOST,
+			database = DB_NAME,
+			user = DB_USER,
+			password = DB_PASS,
+			port = DB_PORT,
+			)
+
+		#create a cursor
+		c = conn.cursor()
+		
+		sql_com = "INSERT INTO PLAYER Values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+		ID = (self.root.ids.registration_scr.ids.playerIDTextField.text)
+		name = (self.root.ids.registration_scr.ids.playerNameTextField.text)
+		team = (self.root.ids.registration_scr.ids.playerTeamIDTextField.text)
+		birth = (self.root.ids.registration_scr.ids.playerBirthdayTextField.text)
+		age = (self.root.ids.registration_scr.ids.playerAgeTextField.text)
+		height = (self.root.ids.registration_scr.ids.playerHeightTextField.text)
+		jersey = (self.root.ids.registration_scr.ids.playerJerseyNumberTextField.text)
+		goals = (self.root.ids.registration_scr.ids.playerGoalsTextField.text)
+		assists = (self.root.ids.registration_scr.ids.playerAssistsTextField.text)
+		reds = (self.root.ids.registration_scr.ids.playerRedCardsTextField.text)
+		yellows = (self.root.ids.registration_scr.ids.playerYellowCardsTextField.text)
+		variable = [ID, name, team, birth, age, height, jersey, goals, assists, reds, yellows]
+		c.execute(sql_com, variable)
+		conn.commit()
+		conn.close()
+	
+	
 	def submit(self):
 		#define the database
 		DB_HOST = "/tmp"
@@ -124,16 +176,23 @@ class TeamXApp(MDApp):
 
 		#add a record
 		#sql_command = "INSERT INTO register (name,teamName,teamID,coachID) Values(%s)" 
-		sql_command = "INSERT INTO COACH Values(%s, %s, %s)" 
+		sql_command1 = "INSERT INTO COACH Values(%s, %s, %s)" 
+		sql_command2 = "INSERT INTO TEAM Values(%s, %s, %s)"
 		coachID = (self.root.ids.registration_scr.ids.coachIDTextField.text)
+		coachTID = (self.root.ids.registration_scr.ids.coachTeamIDTextField.text)
 		name1 = (self.root.ids.registration_scr.ids.nameTextField.text,)
 		teamID = (self.root.ids.registration_scr.ids.teamIDTextField.text)
-		varrrs = [coachID, name1, teamID]
-
+		teamname = (self.root.ids.registration_scr.ids.teamNameTextField.text)
+		wlratio = (self.root.ids.registration_scr.ids.winLossTextField.text)
+		teamCID = (self.root.ids.registration_scr.ids.teamCoachIDTextField.text)
+		
+		variable1 = [coachID, name1, coachTID]
+		variable2 = [teamID, teamname, teamCID]
 
 		#execute postsql command
 		#c.execute(sql_command, name, teamName, teamID, coachID)
-		c.execute(sql_command, varrrs)
+		#c.execute(sql_command2, variable2)
+		c.execute(sql_command1, variable1)
 
 		#viewINFO in the registertion page
 		self.root.ids.registration_scr.ids.viewINFO_label.text = f'{self.root.ids.registration_scr.ids.nameTextField.text} Added'
@@ -149,6 +208,37 @@ class TeamXApp(MDApp):
 
 		# Close our connection
 		conn.close()
+
+	def showPlayers(self):
+		#define the database
+		DB_HOST = "/tmp"
+		DB_NAME = "moonica"
+		DB_USER = "moonica"
+		DB_PASS = "0010"
+		DB_PORT = "8888"
+
+		#create a database or connect to one
+		conn = psycopg2.connect(
+			host = DB_HOST,
+			database = DB_NAME,
+			user = DB_USER,
+			password = DB_PASS,
+			port = DB_PORT,
+			)
+		
+		#create a cursor
+		c = conn.cursor()
+
+		c.execute("SELECT * FROM PLAYER WHERE PLAYER_TEAMID = 0")
+		records = c.fetchall()
+		word = ''
+		for record in records:
+			word = f'{word}\n{record[0]}'
+			self.root.ids.registration_scr.ids.playerID_label.text = f'{word}'
+		conn.commit()
+		conn.close()		
+
+
 
 	def showRecords(self):
 		#define the database
